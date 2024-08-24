f1e5a45abe8d0cb1a2cb0d363db2145ff71f1fe0
GAVHANE MAYUR RAJENDRA <2020bec075@sggs.ac.in>
2024-08-24 23:18:28 +0530

    user activation is added using otp

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -67,8 +67,9 @@ export const registrationUser = CatchAsyncError(
       }
     }
   );
-  
-  
+
+
+ 
 
 interface IActivationToken {
     token: string;
@@ -91,3 +92,44 @@ interface IActivationToken {
   
     return { token, activationCode };
   };
+
+   //acivate User
+
+
+   interface IActivationRequest{
+    activation_token : string;
+    activation_code : string;
+  }
+  
+
+  export const activateUser =  CatchAsyncError(async(req:Request, res:Response, next:NextFunction)=>{
+    try{
+        const {activation_token,activation_code } = req.body as IActivationRequest;
+
+        const newUser : {user: IUser; activationCode:string} =  jwt.verify(
+          activation_token, 
+          process.env.ACTIVATION_SECRET  as string
+        ) as {user: IUser; activationCode : string}
+
+        if(newUser.activationCode !== activation_code){
+          return next(new ErrorHandler("Invalid activation Code",400))
+        }
+      const {name, email, password} = newUser.user
+      const existUser =  await userModel.findOne({email});
+      if(existUser){
+        return next(new ErrorHandler("Email already Exist", 400))
+      }
+
+      const user =  await userModel.create({
+        name, email, password
+      });
+
+      res.status(201).json({
+        success : true
+
+      })
+    }catch(error:any){
+      return next(new ErrorHandler(error.message, 400))
+    }
+  })
+  
