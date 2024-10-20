7ce686e5facfd51887179fac62e655b9d87bd810
GAVHANE MAYUR RAJENDRA <2020bec075@sggs.ac.in>
2024-10-20 19:32:30 +0530

    notifications controller are created

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,7 @@ import { ErrorMiddleware } from './middleware/error';
 import userRouter from './routes/user.route';
 import courseRouter from './routes/course.route';
 import orderRouter from './routes/order.route';
+import notificationRouter from './routes/notification.route';
 
 // Body parser
 app.use(express.json({ limit: "50mb" }));
@@ -20,7 +21,8 @@ app.use(cors({
     origin: process.env.ORIGIN
 }));
 
-app.use("/api/v1", userRouter, courseRouter, orderRouter)
+app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter)
+
 
 // Testing API
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
