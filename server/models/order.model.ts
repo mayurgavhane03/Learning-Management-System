0388e804d7fb2964f2b69bb688a1d62807791ad5
GAVHANE MAYUR RAJENDRA <2020bec075@sggs.ac.in>
2024-10-20 12:05:29 +0530

    course and notification model created

---
+++ server/models/order.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IOrder extends Document {
+  courseId: string;
+  userId: string;
+  payment_info: object;
+}
+
+const orderSchema = new Schema<IOrder>(
+  {
+    courseId: {
+      type: String,
+      require: true,
+    },
+    userId: {
+      type: String,
+      require: true,
+    },
+    payment_info: {
+      type: Object,
+      // require:true
+    },
+  },
+  { timestamps: true }
+);
+
+const OrderModel: Model<IOrder> = mongoose.model("Order", orderSchema);
+export default OrderModel;
