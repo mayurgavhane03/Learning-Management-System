1ffb323895e74a46fcb6328d3c2ef9749ad5f25d
Mayur Gavhane <mayur@teams.tachyonsys.com.au>
2025-09-21 23:13:14 +0530

    edit, and get layout api are created

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,11 +1,16 @@
 import express from "express"
 import { authorizeRoles, isAutheticated } from "../middleware/auth"
-import { createLayout } from "../controllers/layout.controller"
+import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller"
 
 
 const LayoutRouter =  express.Router()
 
 
-LayoutRouter.post("/create-layout", isAutheticated, authorizeRoles("admin"),createLayout)
+LayoutRouter.post("/create-layout", isAutheticated, authorizeRoles("admin"),createLayout);
+
+LayoutRouter.put("/edit-layout", isAutheticated, authorizeRoles("admin"),editLayout)
+
+LayoutRouter.get("/get-layout", getLayoutByType)
+
 
 export default LayoutRouter
